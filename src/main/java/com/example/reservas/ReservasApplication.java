6ef17d5bd4d5144f92d0860a2f60c43bc79e5c15
b3nj4hb <benjalive12@gmail.com>
2023-12-01 13:34:17 -0500

    Add cors config for angular 4200 port

--- src/main/java/com/example/reservas/ReservasApplication.java
+++ src/main/java/com/example/reservas/ReservasApplication.java
@@ -2,8 +2,11 @@ package com.example.reservas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 // import org.springframework.context.annotation.ComponentScan;
 // import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 // @EnableJpaRepositories("com.example.viajes.repository")
@@ -16,6 +19,27 @@ public class ReservasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReservasApplication.class, args);
 		// System.out.println("Hola mundo");
+	}
+
+	// @Bean
+	// public WebMvcConfigurer corsConfigurer() {
+	// return new WebMvcConfigurer() {
+	// @Override
+	// public void addCorsMappings(CorsRegistry registry) {
+	// registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*")
+	// .allowedHeaders("*");
+	// }
+	// };
+	// }
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
--- src/main/java/com/example/reservas/controller/ReservaController.java
+++ src/main/java/com/example/reservas/controller/ReservaController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,6 +42,7 @@ public class ReservaController {
     }
 
     // LISTAR
+    // @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/all")
     public ResponseEntity<List<Reserva>> list() {
         try {
